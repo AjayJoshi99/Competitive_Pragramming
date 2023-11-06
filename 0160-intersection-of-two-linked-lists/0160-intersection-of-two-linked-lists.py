88555b5e6eeabd54a7df606900715a25d4533b18
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-11-06 15:13:12 +0530

    Time: 127 ms (56.02%), Space: 31.3 MB (91.32%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,7 +6,45 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        s1 = set()
+        c1=c2=0
+        temp1,temp2=headA,headB
+        #calculate no. of nodes in each list
+        while temp1 or temp2:
+            if temp1:
+                c1+=1
+                temp1=temp1.next
+            if temp2:
+                c2+=1
+                temp2=temp2.next
+        #find difference ans check which list has more nodes, move head of that list  in order to make both list's pointer to same position.   
+        c=c1-c2
+        if c<0:
+            while c!=0:
+                headB=headB.next
+                c+=1
+        else:
+            while c!=0:
+                headA=headA.next
+                c-=1
+                
+        #both pointer will start to maving ahead by checking if their are equal. 
+        while headA:
+            if headA==headB:
+                return headA
+            headA=headA.next
+            headB=headB.next
+        return headA
+        
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        '''s1 = set()
         
         ptr1 = headA
         ptr2 = headB
@@ -20,4 +58,4 @@ class Solution:
                 return ptr2
             ptr2 = ptr2.next
                 
-        return None
+        return None '''
