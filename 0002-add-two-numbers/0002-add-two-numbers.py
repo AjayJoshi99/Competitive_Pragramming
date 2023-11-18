ea2256498b4a78e12001888eb3578a622412ade6
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-11-18 14:28:35 +0530

    Time: 58 ms (87.69%), Space: 16.4 MB (32.54%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -9,9 +9,9 @@ class Solution:
         tail = dummyHead
         carry = 0
 
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
+        while l1 or l2 or carry != 0:
+            digit1 = l1.val if l1 else 0
+            digit2 = l2.val if l2  else 0
 
             sum = digit1 + digit2 + carry
             digit = sum % 10
@@ -21,8 +21,8 @@ class Solution:
             tail.next = newNode
             tail = tail.next
 
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
         result = dummyHead.next
         dummyHead.next = None
