ac5a05aa5dda8840941fae81b255c350964bb8bd
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-11-21 14:55:41 +0530

    Time: 54 ms (51.34%), Space: 18.9 MB (41.07%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -7,20 +7,33 @@
 from queue import Queue
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        ans = list()
-        l = dict()
-        q = Queue()
-        q.put((root,0))
-        while not q.empty() :
-            node,level = q.get()
-            if level in l :
-                l[level].append(node.val)
-            else :
-                l.update({level:[node.val]})    
-            if node.left : q.put((node.left,level+1))
-            if node.right : q.put((node.right, level + 1))
-                
-        for i in l :
-            ans.append(sum(l[i])/len(l[i]))
-            
-        return ans
+        res = []
+        res = [root.val] 
+        queue = collections.deque()
+        if root.right:
+            queue.append((root.right, 1))
+        if root.left:
+            queue.append((root.left, 1))
+        if len(queue) == 0: return res
+        prevLevel = 1
+        currSum = 0
+        currCount = 0
+        while queue:
+            node = queue.popleft()
+            val, level = node[0].val, node[1]
+            if level != prevLevel:
+                res.append(currSum / currCount)
+                currSum = val
+                currCount = 1
+                prevLevel = level
+            else:
+                currSum += val
+                currCount += 1
+
+            if node[0].left:
+                queue.append((node[0].left, level + 1))
+            if node[0].right:
+                queue.append((node[0].right, level + 1))
+
+        res.append(currSum / currCount)
+        return res
