e7380c06274b699105bda589fd7df40e7275e09f
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-11-25 15:29:35 +0530

    Time: 70 ms (92.40%), Space: 18 MB (20.69%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -6,24 +6,42 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root1 and not root2 : return None
-        if not root1 and root2 : return root2
-        if not root2 and root1 : return root1    
-        def createTree(r1,r2,r):
-            if not r1 and not r2 : r = None
-            elif r1 and not r2 : r = TreeNode(r1.val)
-            elif r2 and not r1 : r = TreeNode(r2.val)
-            else : r = TreeNode(r1.val+r2.val)
-            if r1 and r2 :
-                r.left = createTree(r1.left,r2.left,r)
-                r.right = createTree(r1.right,r2.right,r)
-            elif not r1 and r2 :
-                r.left = createTree(None,r2.left,r)
-                r.right = createTree(None,r2.right,r)
-            elif r1 and not r2 :
-                r.left = createTree(r1.left,None,r)
-                r.right = createTree(r1.right,None,r)
-            return r
-        return createTree(root1,root2,None)
+        def dfs(node1, node2):
+            if node1 and node2:
+                root = TreeNode(node1.val + node2.val)
+                root.left = dfs(node1.left, node2.left)
+                root.right = dfs(node1.right, node2.right)
+                return root
+            else:
+                return node1 or node2
+        
+        return dfs(root1, root2)
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not root1 and not root2 : return None
+#         if not root1 and root2 : return root2
+#         if not root2 and root1 : return root1    
+#         def createTree(r1,r2,r):
+#             if not r1 and not r2 : r = None
+#             elif r1 and not r2 : r = TreeNode(r1.val)
+#             elif r2 and not r1 : r = TreeNode(r2.val)
+#             else : r = TreeNode(r1.val+r2.val)
+#             if r1 and r2 :
+#                 r.left = createTree(r1.left,r2.left,r)
+#                 r.right = createTree(r1.right,r2.right,r)
+#             elif not r1 and r2 :
+#                 r.left = createTree(None,r2.left,r)
+#                 r.right = createTree(None,r2.right,r)
+#             elif r1 and not r2 :
+#                 r.left = createTree(r1.left,None,r)
+#                 r.right = createTree(r1.right,None,r)
+#             return r
+#         return createTree(root1,root2,None)
         
         
