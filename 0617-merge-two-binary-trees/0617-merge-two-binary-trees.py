1b6d69c246824b375ebe664f727075559737308b
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-11-25 15:27:25 +0530

    Time: 85 ms (24.55%), Space: 18.1 MB (5.27%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -6,21 +6,21 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root1 is None and root2 is None  : return None
-        if  root1 is None  and root2 : return root2
-        if  root2 is None  and root1 : return root1    
+        if not root1 and not root2 : return None
+        if not root1 and root2 : return root2
+        if not root2 and root1 : return root1    
         def createTree(r1,r2,r):
-            if r1 is None and r2 is None : r = None
-            elif r1 and r2 is None : r = TreeNode(r1.val)
-            elif r2 and r1 is None  : r = TreeNode(r2.val)
+            if not r1 and not r2 : r = None
+            elif r1 and not r2 : r = TreeNode(r1.val)
+            elif r2 and not r1 : r = TreeNode(r2.val)
             else : r = TreeNode(r1.val+r2.val)
             if r1 and r2 :
                 r.left = createTree(r1.left,r2.left,r)
                 r.right = createTree(r1.right,r2.right,r)
-            elif r1 is None  and r2 :
+            elif not r1 and r2 :
                 r.left = createTree(None,r2.left,r)
                 r.right = createTree(None,r2.right,r)
-            elif r1 and r2 is None  :
+            elif r1 and not r2 :
                 r.left = createTree(r1.left,None,r)
                 r.right = createTree(r1.right,None,r)
             return r
