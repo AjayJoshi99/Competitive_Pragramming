09773179be9f5c354e90681369b2abbdf26274e9
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-12-03 15:11:08 +0530

    Time: 65 ms (36.28%), Space: 18.3 MB (10.85%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -8,29 +8,16 @@ class Node:
         self.next = next
 """
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root:
-            return None
-        node = root 
-        while node.left:
-            head = node
-            while head:
-                head.left.next = head.right
-                if head.next:
-                    head.right.next = head.next.left
-                head = head.next
-            node = node.left
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':        
+        if not root : return root
+        q = [root]
+        while q:
+            t = len(q)
+            for i in range(t):
+                node = q.pop(0)
+                if i != t-1:
+                    node.next = q[0]    
+                if node.right :
+                    q.append(node.left)
+                    q.append(node.right)  
         return root
-        
-        # if not root : return root
-        # q = [root]
-        # while q:
-        #     t = len(q)
-        #     for i in range(t):
-        #         node = q.pop(0)
-        #         if i != t-1:
-        #             node.next = q[0]    
-        #         if node.right :
-        #             q.append(node.left)
-        #             q.append(node.right)  
-        # return root
