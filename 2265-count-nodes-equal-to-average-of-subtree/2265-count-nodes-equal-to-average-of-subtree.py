0cf941ecef7c0e426e8fe21306b578ff8be5ea0d
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-12-08 17:12:26 +0530

    Time: 45 ms (80.23%), Space: 17.6 MB (54.02%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -9,15 +9,13 @@ class Solution:
         count=0
         def postorder(r):
             nonlocal count
-            if r :
-                s1,n1 = postorder(r.left)
-                s2,n2= postorder(r.right)
-                s =s1+s2+r.val
-                n = n1+n2+1
-                if r.val == s//n:
-                    count += 1
-                return s,n
-            else:
-                return 0,0
+            if not r : return 0,0
+            s1,n1 = postorder(r.left)
+            s2,n2= postorder(r.right)
+            s =s1+s2+r.val
+            n = n1+n2+1
+            if r.val == s//n:
+                count += 1
+            return s,n
         postorder(root)
         return count
