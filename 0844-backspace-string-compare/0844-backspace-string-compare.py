875ae10235995bed868945a054a6d5c378ac3954
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-12-29 22:05:45 +0530

    Time: 40 ms (45.96%), Space: 17.2 MB (9.47%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,14 +1,31 @@
 class Solution:
-    def backspaceCompare(self, S: str, T: str) -> bool:
-        def build(S):
-            ans = []
-            for c in S:
-                if c != '#':
-                    ans.append(c)
-                elif ans:
-                    ans.pop()
-            return "".join(ans)
-        return build(S) == build(T)
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def process_string(string):
+            k = 0
+            for i in range(len(string)):
+                if string[i] != '#':
+                    string[k] = string[i]
+                    k += 1
+                else:
+                    k = max(k-1, 0)
+            return k
+        
+        # Convert strings to lists for in-place modifications
+        s, t = list(s), list(t)
+        
+        k = process_string(s)
+        p = process_string(t)
+        
+        # If effective lengths are different, return False
+        if k != p:
+            return False
+        
+        # Compare the effective characters
+        for i in range(k):
+            if s[i] != t[i]:
+                return False
+        
+        return True
                     
                 
                 
