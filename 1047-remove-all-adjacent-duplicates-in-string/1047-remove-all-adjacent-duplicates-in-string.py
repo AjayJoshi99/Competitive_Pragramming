8be583e21c947714ae45f85728c13563fe7abd76
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-12-30 14:57:50 +0530

    Time: 119 ms (15.22%), Space: 18.3 MB (5.23%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -3,9 +3,9 @@ class Solution:
         ans = []
         for i in range(len(s)):
             if len(ans) and ans[-1] != s[i]:
-                ans += s[i]
+                ans.append(s[i])
             elif not len(ans) :
-                ans += s[i]
+                ans.append(s[i])
             else :
                 while len(ans) and ans[-1] == s[i]:
                     ans.pop()
