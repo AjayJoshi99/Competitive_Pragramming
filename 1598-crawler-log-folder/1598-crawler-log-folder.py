2807774761b8c5825583a47ea937ff123bad044c
Ajay Joshi <ajayjoshi1908@gmail.com>
2023-12-31 14:51:33 +0530

    Time: 107 ms (5.40%), Space: 17.3 MB (14.74%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -2,7 +2,7 @@ class Solution:
     def minOperations(self, logs: List[str]) -> int:
         c = 0 
         for i in logs:
-            if i =='../':
+            if i =='../': 
                 if c : c-=1
             elif i == './': pass
             else: c+=1
