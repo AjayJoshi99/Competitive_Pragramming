41513a66a4cc8b6ed3705fc6fe083898a097be60
Ajay Joshi <ajayjoshi1908@gmail.com>
2024-03-28 13:53:24 +0530

    Time: 1153 ms (59.56%), Space: 31.1 MB (37.58%) - LeetHub

---
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int: 
+        """Sliding window approch"""
+        count = defaultdict(int)
+        res = 0
+        l = 0 #left pointer
+        for r in range(len(nums)): # right pointer
+            count[nums[r]] += 1 
+            while count[nums[r]] > k: 
+                count[nums[l]] -= 1 
+                l += 1 
+            res = max(res, r - l + 1) 
+        return res
