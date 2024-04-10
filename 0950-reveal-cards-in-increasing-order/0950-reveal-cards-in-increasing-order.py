20048e36c65b63f662e0747c881951dd9ea25bfe
Ajay Joshi <ajayjoshi1908@gmail.com>
2024-04-10 18:16:43 +0530

    Time: 70 ms (5.07%), Space: 16.9 MB (9.42%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -5,11 +5,9 @@ class Solution:
         l = len(deck)
         ans = [0]*l
         deck.sort()
-        for i in range(l):
-            q.put(i)
+        for i in range(l): q.put(i)
         for i in range(l):
             ans[q.get()] = deck[i]
-            if not q.empty():
-                q.put(q.get())
+            if not q.empty():q.put(q.get())
         return ans
         
