75027a92bff5099cc2b21f568e7ff4a301f613f8
Ajay Joshi <ajayjoshi1908@gmail.com>
2024-05-10 09:52:14 +0530

    Time: 2379 ms (15.25%), Space: 100.5 MB (21.36%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
@@ -1,11 +1,11 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        d = {}
-        for i in range(len(arr)):
-            for j in range(i+1,len(arr)):
-                d.update({arr[i]/arr[j]:[arr[i],arr[j]]})
-        d = dict(sorted(d.items(),key=lambda x:x[0]))
-        for i, item in enumerate(d):
-            if i == k-1 :
-                return d[item]
+        minHeap = []
+        N = len(arr)
+        for i in range(N):
+            for j in range(i + 1, N):
+                heappush(minHeap, (arr[i] / arr[j], (arr[i], arr[j])))
+        for _ in range(k):
+            a, b = heappop(minHeap)[1]
+        return [a, b]
             
