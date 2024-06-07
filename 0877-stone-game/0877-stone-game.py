d980943920742d28340baaa19caa7607de34ef66
Ajay Joshi <ajayjoshi1908@gmail.com>
2024-06-07 09:38:38 +0530

    Time: 36 ms (86.99%), Space: 16.5 MB (97.21%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,21 +1,11 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        memo = {}
-        def take(left, right):
-            # Look-up DP Table
-            if (left, right) in memo:
-                return memo[(left, right)]
-            # Base case
-            if left == right:
-                return piles[left]
-            # General cases
-            # Option_1
-            take_first_pile = piles[left] - take(left+1, right)
-
-            # Option_2
-            take_last_pile = piles[right] - take(left, right-1)
-
-            memo[(left, right)] = max( take_first_pile,  take_last_pile)
-            return memo[(left, right)]
-        
-        return take(0, len(piles)-1) > 0
+        a,b = 0,0
+        l = 0
+        h = len(piles)-1
+        while l<h:
+            a += max(piles[l],piles[h])
+            b += min(piles[l],piles[h])
+            l += 1
+            h -= 1
+        return a>b
