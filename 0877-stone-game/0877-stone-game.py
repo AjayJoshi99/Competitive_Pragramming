737ae80d5230e6753f5b33baf9cd5fb07718df35
Ajay Joshi <ajayjoshi1908@gmail.com>
2024-06-07 09:42:12 +0530

    Time: 428 ms (16.59%), Space: 96.6 MB (25.94%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,11 +1,28 @@
 class Solution:
-    def stoneGame(self, piles: List[int]) -> bool:
-        a,b = 0,0
-        l = 0
-        h = len(piles)-1
-        while l<h:
-            a += max(piles[l],piles[h])
-            b += min(piles[l],piles[h])
-            l += 1
-            h -= 1
-        return a>b
+    def stoneGame(self, nums: List[int]) -> bool:
+        dp = {}
+        def dfs(l,r,alic_turn):
+
+            if (l,r,alic_turn) in dp:
+                return dp[(l,r,alic_turn)]
+                
+            if l >= r:
+                return 0
+
+            a = 0
+            b = 0
+            if alic_turn:
+                # alic takes from front or from back, 
+                # whatever one leads to a better score thats why max()
+                a = max(dfs(l,r-1, not alic_turn)+nums[r] , dfs(l+1,r, not alic_turn)+nums[l])
+            else:
+                # bob takes from front or from back, 
+                # whatever one leads to a better score  
+                b = max(dfs(l,r-1, not alic_turn) +nums[r], dfs(l+1,r, not alic_turn)+nums[l])
+            
+
+            dp[(l,r,alic_turn)]  = a > b
+            return a > b
+
+
+        return  dfs(0,len(nums)-1,True)
