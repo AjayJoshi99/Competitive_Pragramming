dfd75c9bcf0dffdc6536f93a28ef25ff99c2697f
Ajay Joshi <ajayjoshi1908@gmail.com>
2024-06-08 15:00:11 +0530

    Time: 26 ms (97.25%), Space: 16.5 MB (39.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,5 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+        
+        return dp[-1] 
+""" 
         l = len(nums)
         dp = [-1]*l
         def helper(idx):
@@ -10,3 +22,4 @@ class Solution:
             dp[idx] = max(a,b)
             return dp[idx]
         return helper(0)
+"""
