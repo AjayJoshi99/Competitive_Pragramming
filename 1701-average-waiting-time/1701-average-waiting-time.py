5a655233fdd70699e052230509110d6c18e87ee7
Ajay Joshi <ajayjoshi1908@gmail.com>
2024-07-09 19:01:54 +0530

    Time: 727 ms (92.44%), Space: 57.2 MB (45.36%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.py
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -1,14 +1,13 @@
 class Solution:
     def averageWaitingTime(self, l: List[List[int]]) -> float:
-        # sorted(l,key=lambda l:l[0])
         total = 0
         time = l[0][0]
         for i in l:
             if i[0]<=time:
-                total += (time-i[0]+i[1])
+                total += time-i[0]+i[1]
                 time += i[1]
             else:
                 total += i[1]
-                time += i[0]-time + i[1]            
+                time = i[0] + i[1]            
         return total/len(l)
             
