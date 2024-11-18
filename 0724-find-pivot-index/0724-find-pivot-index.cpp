a7c5dba67dbad8c47fd06b7905d302145cab2260
Ajay Joshi <ajayjoshi1908@gmail.com>
2024-11-18 09:39:52 +0530

    Time: 0 ms (100.00%), Space: 34.5 MB (87.97%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        vector<int> prefix(nums.size()),postfix(nums.size());
-        for(int i = nums.size()-2; i>-1; i--){
-            postfix[i] = postfix[i+1] + nums[i+1];
+//         vector<int> prefix(nums.size()),postfix(nums.size());
+//         for(int i = nums.size()-2; i>-1; i--){
+//             postfix[i] = postfix[i+1] + nums[i+1];
             
-        }
-        for(int i =1; i<nums.size();i++){
-            prefix[i] = prefix[i-1]+nums[i-1];
+//         }
+//         for(int i =1; i<nums.size();i++){
+//             prefix[i] = prefix[i-1]+nums[i-1];
             
+//         }
+//         for(int i =0; i<nums.size();i++){
+//            if(postfix[i]==prefix[i]) return i;
+//         }
+        
+        int total=0,left=0;
+        for(int val: nums){
+            total+=val;
         }
-        for(int i =0; i<nums.size();i++){
-           if(postfix[i]==prefix[i]) return i;
+        for(int i =0; i<nums.size(); i++){
+            if(left == total-left-nums[i]) return i;
+            left+=nums[i];
         }
         return -1;
     }
