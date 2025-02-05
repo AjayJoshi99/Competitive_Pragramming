fc0421406211c1b7a0d805b0d43cbcc43dce96af
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-02-05 22:08:02 +0530

    Time: 2 ms, Memory: 8.5 MB - LeetHub

---
+++ 1520-73-1790-check-if-one-string-swap-can-make-strings-equal/1520-73-1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        return helper(s1,s2) || helper(s2,s1);
+    }
+    bool helper(string s1, string s2){
+        int l = s1.length(), flag = 0,count=0;
+        char c1=' ', c2=' ';
+        for(int i =0; i<l; i++){
+            if(s1[i]!=s2[i] && flag == 0){
+                flag = 1;
+                c1 = s1[i];
+                c2 = s2[i];
+                count++;
+                if(count>2) return false;
+            }else if(flag==1 && s1[i]!=s2[i]){
+                if(!(c1==s2[i]&&c2==s1[i])){
+                    return false;
+                }
+                count++;
+            }
+        }
+        if(count==1 || count >2)  return false;
+        return true;
+    }
+};
