d177c652b9d56151c40fd683e8dbd699aac78a9e
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-02-21 15:45:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -1,49 +1,48 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
 
-class Solution
-{
-    public:
-    //Function to check if brackets are balanced or not.
-    bool ispar(string x)
-    {
-        int len = 0;
-        for(char ch: x) {
-            if( ch == '(' || ch == '{' || ch == '[') 
-                x[len++] = ch;
-            else if(ch == '}' && x[len-1] == '{') 
-                len--;
-            else if(ch == ')' && x[len-1] == '(') 
-                len--;
-            else if(ch == ']' && x[len-1] == '[') 
-                len--;
-            else 
-                return false;
-        }
-        return len == 0;
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+       stack<char> stack;
+       for( char c : s){
+           if(c == '{' || c == '(' || c ==  '['){
+               stack.push(c);
+           }else{
+               if(stack.size()==0) return false;
+               char last = stack.top();
+               stack.pop();
+               if(c == ']' && last != '[') return false;
+               else if( c == '}' && last != '{') return false;
+               else if(c == ')' && last != '(') return false;
+           }
+       }
+       if(stack.size()==0) return true;
+       return false;
     }
-
 };
+
 
 //{ Driver Code Starts.
 
-int main()
-{
-   int t;
-   string a;
-   cin>>t;
-   while(t--)
-   {
-       cin>>a;
-       Solution obj;
-       if(obj.ispar(a))
-        cout<<"balanced"<<endl;
-       else
-        cout<<"not balanced"<<endl;
-   }
+int main() {
+    int t;
+    string a;
+    cin >> t;
+    while (t--) {
+        cin >> a;
+        Solution obj;
+        if (obj.isBalanced(a))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+
+        cout << "~"
+             << "\n";
+    }
 }
 // } Driver Code Ends
