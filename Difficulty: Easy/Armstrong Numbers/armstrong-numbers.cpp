59de50393c22d5bf469b57104529dd3b739feb2c
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-06-03 19:14:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -0,0 +1,13 @@
+// User function Template for C++
+class Solution {
+  public:
+    bool armstrongNumber(int n) {
+        int sum = 0, num = n;
+        while(n){
+            sum += pow(n%10,3);
+            n/=10;
+        }
+        return sum==num;
+        
+    }
+};
