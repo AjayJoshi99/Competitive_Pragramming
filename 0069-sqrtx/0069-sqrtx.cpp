e4def0685bde90a43e0e70cc4df0687b8adf1fd9
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-06-14 09:47:19 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.94%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int st = 1, end = x, ans = 0;
+        while(st<=end){
+            int mid = st + (end - st)/2;
+            if(mid<= x/mid ){
+                st = mid +1;
+                ans = mid;
+            }else{
+                end = mid -1;
+            }
+        }
+        return ans;
+    }
+};
