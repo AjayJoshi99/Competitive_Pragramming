9f8dfb4f2417ec65c131c59a6209fdec8e6b30e0
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-06-14 09:47:54 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.8%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,13 +4,13 @@ public:
         int st = 1, end = x, ans = 0;
         while(st<=end){
             int mid = st + (end - st)/2;
-            if(mid<= x/mid ){
+            if(mid <= x/mid ){
                 st = mid +1;
                 ans = mid;
             }else{
                 end = mid -1;
             }
         }
-        return ans;
+        return end;
     }
 };
