93e0b75417fa1b77323127e94673d9505076f0ed
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-06-24 07:23:06 +0530

    Time: 1 ms (73.11%), Space: 9.1 MB (14.63%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<1) return false;
+        double base3 = (double)log(n)/log(3);
+        return abs(base3 - round(base3)) < 1e-10;
+    }
+};
