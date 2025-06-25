2229655453bf509dfeb2de855e385b4a64157150
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-06-25 15:10:28 +0530

    Time: 0 ms (100%), Space: 8.2 MB (8.03%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int findComplement(int num) {
         int i = floor(log2(num)) + 1, temp = 1;
-        while(i--){
-            num = num^temp;
-            temp <<= 1;
-        }
-        return num;
+        int x = pow(2,i)-1;
+        return x^num;
+       
     }
 };
