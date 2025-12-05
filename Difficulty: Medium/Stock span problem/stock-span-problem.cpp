51debc470753dbf7ca7349bfe457eee01a852b13
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-12-05 10:56:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.cpp
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -1,53 +1,15 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> calculateSpan(vector<int>& arr) {
-        int n=arr.size();
-        stack<int>st;
-        vector<int>v(n,1);
-         
-        for(int i=0;i<n;i++){
-                while(!st.empty() and arr[i]>=arr[st.top()]){
-                      v[i]+=v[st.top()];
-                      st.pop();
-                }
-            st.push(i);
-            
+        stack<int> s;
+        vector<int> ans(arr.size());
+        for(int i =0; i<arr.size(); i++){
+            while(!s.empty() &&  arr[s.top()]<=arr[i]){
+                s.pop();
+            }
+            ans[i] = !s.empty() ? i-s.top() : i+1;
+            s.push(i);
         }
-        return v;
+        return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        vector<int> ans = obj.calculateSpan(arr);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
