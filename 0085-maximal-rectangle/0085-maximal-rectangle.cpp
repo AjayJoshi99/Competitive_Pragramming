c5c48c129c6e3f015c301f9cc619d03efe7d7f3a
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-12-05 10:23:50 +0530

    Time: 16 ms (14.42%), Space: 21.8 MB (12.7%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,28 +1,58 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty())
-            return 0;
+    vector<int> prevSmaller(vector<int>& h) {
+        int n = h.size();
+        vector<int> prev(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && h[st.top()] >= h[i])
+                st.pop();
+            prev[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return prev;
+    }
 
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<int> heights(cols + 1, 0); 
+    vector<int> nextSmaller(vector<int>& h) {
+        int n = h.size();
+        vector<int> next(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && h[st.top()] >= h[i])
+                st.pop();
+            next[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return next;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) 
+            return 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> height(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1')
+                    height[i][j] = (i == 0 ? 1 : height[i-1][j] + 1);
+                else
+                    height[i][j] = 0;
+            }
+        }
+
         int maxArea = 0;
 
-        for (const auto& row : matrix) {
-            for (int i = 0; i < cols; i++) {
-                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
-            }
+        for (int i = 0; i < n; i++) {
+            vector<int>& h = height[i];
 
-            stack<int> stk;
-            for (int i = 0; i < heights.size(); i++) {
-                while (!stk.empty() && heights[i] < heights[stk.top()]) {
-                    int h = heights[stk.top()];
-                    stk.pop();
-                    int w = stk.empty() ? i : i - stk.top() - 1;
-                    maxArea = max(maxArea, h * w);
-                }
-                stk.push(i);
+            vector<int> prev = prevSmaller(h);
+            vector<int> next = nextSmaller(h);
+
+            for (int j = 0; j < m; j++) {
+                int width = next[j] - prev[j] - 1;
+                int area = h[j] * width;
+                maxArea = max(maxArea, area);
             }
         }
 
