642adb1a6831c203d0853db1a101a7f9da202c5c
Ajay Joshi <ajayjoshi1908@gmail.com>
2025-12-30 14:28:35 +0530

    Time: 27 ms (57.53%), Space: 19 MB (50.22%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dp;
+
+    int solve(int amount, vector<int>& coins) {
+        if (amount == 0) return 0;
+        if (amount < 0) return 1e9;
+
+        if (dp[amount] != -1)
+            return dp[amount];
+
+        int ans = 1e9;
+        for (int coin : coins) {
+            ans = min(ans, 1 + solve(amount - coin, coins));
+        }
+
+        return dp[amount] = ans;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        dp.assign(amount + 1, -1);
+        int res = solve(amount, coins);
+        return (res >= 1e9) ? -1 : res;
+    }
+};
