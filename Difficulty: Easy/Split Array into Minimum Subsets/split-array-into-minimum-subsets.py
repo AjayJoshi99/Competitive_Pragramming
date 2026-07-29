8b6602e7b5804b4c8eeba5757523cf11ad52987a
Ajay Joshi <ajayjoshi1908@gmail.com>
2026-07-29 07:15:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Array into Minimum Subsets/split-array-into-minimum-subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSubsets(self, arr):
+        arr.sort()
+        
+        count = 0
+        
+        for i in range(1, len(arr)):
+            if arr[i]-arr[i-1] != 1 :
+                count += 1
+        else :
+            count += 1
+        
+        return count
+        
